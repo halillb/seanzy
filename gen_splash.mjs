e0f8085742eddf9bbc8920696890997985bdbe20
Halil Bulbul <halilbulbul93@gmail.com>
2026-07-12 01:31:41 +0300

    Yeni Seanzy logosunu (altin S, koyu/acik varyant) ikon ve splash varliklarina uygula

--- gen_splash.mjs
+++ gen_splash.mjs
@@ -1,11 +1,8 @@
 import sharp from 'sharp'
-import { readFileSync } from 'fs'
-
-const svg = readFileSync('public/icon.svg')
 
 async function main() {
   const bg = { r: 0x0c, g: 0x0c, b: 0x0d, alpha: 1 }
-  const mark = await sharp(svg, { density: 384 }).resize(640, 640).png().toBuffer()
+  const mark = await sharp('public/logo-kaynak/logo-transparent.png').resize(640, 640).png().toBuffer()
   await sharp({ create: { width: 2732, height: 2732, channels: 4, background: bg } })
     .composite([{ input: mark, left: Math.round((2732 - 640) / 2), top: Math.round((2732 - 640) / 2) }])
     .png().toFile('resources/splash.png')
